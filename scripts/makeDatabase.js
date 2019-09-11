2a8cbd38e1f2b0f9bb277700fab34fcd52786708
Nick Cummings <nickmcummings@gmail.com>
2019-09-11 16:35:19 -0700

    Stubbed out notes for dev.

---
+++ scripts/makeDatabase.js
@@ -0,0 +1,2 @@
+// TODO: This thing should take data/raw.json and pare it down to something very simple.
+// It should also run as part of a build script, if I get that far. But it should look for a songs.json file first before overwriting!
