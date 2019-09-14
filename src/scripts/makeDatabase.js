3e5356b1d18c6837d1faa59564596f7ee114678d
Nick Cummings <nickmcummings@gmail.com>
2019-09-13 23:11:48 -0700

    Add deduplication logic to DB generator script.

--- src/scripts/makeDatabase.js
+++ src/scripts/makeDatabase.js
@@ -20,11 +20,8 @@ songs.map(song => {
 
     if (simplifiedFieldsToUse[i] === "title") {
       // Cleanup titles
-
-      text = text.match(/([\w\d\s'\/\.!?]+)/);
-
+      text = text.match(/([\w\d\s'\/\.!¿?:;,]+)/);
       text = text[1];
-      console.log(text);
     }
 
     // Mix Pop/Rock and Pop-Rock together
@@ -43,7 +40,25 @@ songs.map(song => {
   simplifiedSongs.push(simplifiedSong);
 });
 
-fs.writeFile("./src/data/db.json", JSON.stringify(simplifiedSongs), err => {
+// Deduplicate songs
+let deduplicatedSongs = [];
+
+for (let i = 0; i < simplifiedSongs.length; i++) {
+  // Check to see if song is already in deduped array. Only add if it's not.
+  const index = deduplicatedSongs.findIndex(
+    song => song.title === simplifiedSongs[i].title && song.artist === simplifiedSongs[i].artist
+  );
+  if (index > -1) {
+    console.log(`${simplifiedSongs[i].title} - ${simplifiedSongs[i].artist} already in array - skipping`);
+  } else {
+    deduplicatedSongs.push(simplifiedSongs[i]);
+  }
+}
+
+console.log(simplifiedSongs.length, "songs in source file");
+console.log(deduplicatedSongs.length, "songs minus duplicates");
+
+fs.writeFile("./src/data/db.json", JSON.stringify(deduplicatedSongs), err => {
   if (err) throw err;
   console.log("Database created!");
 });
