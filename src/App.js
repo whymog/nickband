404366a5a4e8871f401d8eef7a7b3f12b30aecd2
Nick Cummings <nickmcummings@gmail.com>
2019-09-13 19:12:08 -0700

    Basic filtering.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,53 @@ import Song from "./components/Song/index";
 import db from "./data/db.json";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>NickBand v0.1</h1>
-      {db.map(song => (
-        <Song {...song} />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filters: {
+        isOwned: true,
+        genre: "",
+        searchString: ""
+      }
+    };
+  }
+
+  getFilteredSongs() {
+    const { filters } = this.state;
+
+    let filteredSongs = [];
+
+    db.forEach(song => {
+      if (filters.isOwned && song.owned !== "Y") return;
+      if (filters.genre.length && filters.genre.length !== song.genre) return;
+      if (
+        filters.searchString.length &&
+        !song.title.match(filters.searchString) &&
+        !song.artist.match(filters.searchString)
+      )
+        return;
+
+      filteredSongs.push(song);
+    });
+
+    return filteredSongs;
+  }
+
+  render() {
+    const filteredSongs = this.getFilteredSongs();
+    console.log(filteredSongs.length);
+
+    return (
+      <div className="App">
+        <h1>NickBand v0.1</h1>
+        {filteredSongs.map((song, i) => (
+          <Song key={i} {...song} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
